e10fd069e383f8cd428ed88e2874edc0af9cc729
swiftcode <mpc@junct.com>
2018-08-28 15:43:35 -0500

    feat WebView-001: Create WebView for CDG site
    Create WebView as a UIView.  Moved webview code from the view controller to the view.
    
    References #1
    
    Changes to be committed:
    	modified:   Mercury-iOS.xcodeproj/project.pbxproj
    	modified:   Mercury-iOS/Controllers/MainViewController.swift
    	new file:   Mercury-iOS/Views/WebView.swift

--- Mercury-iOS/Controllers/MainViewController.swift
+++ Mercury-iOS/Controllers/MainViewController.swift
@@ -8,46 +8,20 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UIWebViewDelegate  {
+class MainViewController: UIViewController  {
 
-    var webView: UIWebView?
-    var activityIndicator: UIActivityIndicatorView?
+    var webView: WebView?
+
+    override func viewWillAppear(_ animated: Bool) {
+        let frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: self.view.frame.height)
+        
+        webView = WebView(frame: frame)
+        guard let webView = webView else { return }
+        
+        self.view.addSubview(webView)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let frame = CGRect(x: 100.0, y: 100.0, width: 40.0, height: 40.0)
-        
-        activityIndicator = UIActivityIndicatorView(frame: frame)
-        activityIndicator?.center = self.view.center
-        activityIndicator?.isHidden = true
-        
-        webView = UIWebView(frame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: self.view.frame.height))
-        
-        guard let webView = webView else { return }
-        
-        webView.delegate = self
-        self.view.addSubview(webView)
-        
-        if let url = URL(string: Organization.website) {
-            let request = URLRequest(url: url)
- 
-            DispatchQueue.main.async {
-                webView.loadRequest(request)
-                self.activityIndicator?.isHidden = false
-            }
-        }
-    }
-
-    func webViewDidStartLoad(_ webView: UIWebView){
-        activityIndicator?.color = .red
-        activityIndicator?.alpha = 1
-        activityIndicator?.isHidden = false
-        activityIndicator?.startAnimating()
-    }
-    
-    func webViewDidFinishLoad(_ webView: UIWebView){
-        activityIndicator?.stopAnimating()
-        activityIndicator?.isHidden = true
     }
 }
---
+++ Mercury-iOS/Views/WebView.swift
@@ -0,0 +1,61 @@
+//
+//  WebView.swift
+//  Mercury-iOS
+//
+//  Created by mpc on 8/28/18.
+//  Copyright © 2018 mpc. All rights reserved.
+//
+
+import UIKit
+
+class WebView: UIView, UIWebViewDelegate {
+
+    var view: UIView?
+    var webView: UIWebView?
+    var activityIndicator = UIActivityIndicatorView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        /* Setup main view for window */
+        view = UIView(frame: frame)
+        guard let view = view else { return }
+        addSubview(view)
+        
+        /* Setup webView to display organization website */
+        webView = UIWebView(frame: frame)
+        guard let webView = webView else { return }
+        webView.delegate = self
+        view.addSubview(webView)
+        
+        /* Setup activity indicator to display while page is loading */
+        activityIndicator.frame = CGRect(x: 0.0, y: 0.0, width: 40.0, height: 40.0)
+        activityIndicator.hidesWhenStopped = true
+        view.addSubview(activityIndicator)
+        activityIndicator.center = view.center
+        
+        /* Load organization website in webview */
+        if let url = URL(string: Organization.website) {
+            let request = URLRequest(url: url)
+            
+            DispatchQueue.main.async {
+                webView.loadRequest(request)
+            }
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView){
+        //activityIndicator.isHidden = false
+        activityIndicator.color = .black
+        activityIndicator.alpha = 1.0
+        activityIndicator.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView){
+        activityIndicator.stopAnimating()
+    }
+}
