b95bcebb64837cd94fd185b264c5957955d54883
swiftcode <mpc@junct.com>
2018-08-29 13:45:40 -0500

    style WebView-001: Create WebView for CDG site
    Tweaks to the activity indicator that increases the sized based on device scale.
    
    References #1
    
    Changes to be committed:
    	modified:   Mercury-iOS.xcodeproj/project.pbxproj
    	new file:   Mercury-iOS/Utils/Utils.swift
    	modified:   Mercury-iOS/Views/WebView.swift

---
+++ Mercury-iOS/Utils/Utils.swift
@@ -0,0 +1,14 @@
+//
+//  Utils.swift
+//  Mercury-iOS
+//
+//  Created by mpc on 8/29/18.
+//  Copyright © 2018 mpc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var scaleFactor: CGFloat {
+    return UIScreen.main.scale
+}
